445932043524847725499b7888f013bb3064c440
Wimolchai <Oattowimolchai@gmail.com>
2026-02-23 16:29:27 +0700

    02

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -4,6 +4,12 @@ import { PrismaClient } from '@prisma/client'
 const globalForPrisma = global as unknown as { prisma: PrismaClient }
 
 // ✅ ต้องมี export ตรงนี้
-export const prisma = globalForPrisma.prisma || new PrismaClient()
+export const prisma =
+  globalForPrisma.prisma ||
+  new PrismaClient({
+    log: ['error'],
+  })
 
-if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
+if (process.env.NODE_ENV !== 'production') {
+  globalForPrisma.prisma = prisma
+}
